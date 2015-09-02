29ea150250f62f92b578c44d59348ea76b9f5e49
Simon Gibbons <simongibbons@gmail.com>
2015-09-02 17:18:25 +0100

    Minor tweak for problem9

--- solutions/set2/problem9.cc
+++ solutions/set2/problem9.cc
@@ -1,4 +1,5 @@
 #include <util/padding.h>
+#include <util/encoding.h>
 
 #include <string>
 #include <iostream>
@@ -6,14 +7,14 @@
 int main()
 {
     std::string s = "YELLOW SUBMARINE";
+    std::vector<uint8_t> s_bytes(s.begin(), s.end());
     unsigned int block_size = 20;
 
     std::string padded = pkcs7_pad(s, block_size);
 
-    std::cout << "Input String: " << s << std::endl;
-    std::cout << "Input String Length: " << s.size() << std::endl;
-    std::cout << "Block Size: " << block_size << std::endl;
+    std::vector<uint8_t> padded_bytes(padded.begin(), padded.end());
 
-    std::cout << "Padded String: " << padded << std::endl;
-    std::cout << "Padded String Length: " << padded.size() << std::endl;
+    std::cout << "Input String: " << s << std::endl;
+    std::cout << "Input String Hex Encoded:  " << encode_hex(s_bytes) << std::endl;
+    std::cout << "Padded String Hex Encoded: " << encode_hex( padded_bytes ) << std::endl;
 }
